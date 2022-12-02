0a322f97c3528cdc03eeabb07f08aaac945fbe30
chiragdhruve01 <chirag@houstonsupport.com>
2022-12-02 09:23:52 +0530

    added admin views

--- travel/admin.py
+++ travel/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.Country)
+admin.site.register(models.State)
+admin.site.register(models.City)
+admin.site.register(models.Area)
